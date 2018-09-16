e0b89e1bcfbc9f8fe9620a361291b2729c40d00f
Ammad Akhtar <ammadakhtar@live.com>
2018-09-16 11:47:37 +0500

    project architecture setup and added git ignore file

---
+++ ios-tech-challenge/Constants/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ios-tech-challenge
+//
+//  Created by Ammad Akhtar on 16/09/2018.
+//  Copyright © 2018 Ammad Akhtar. All rights reserved.
+//
+
+import Foundation
+
+let kBaseUrl = "http://192.168.10.8:8080/deliveries"
---
+++ ios-tech-challengeTests/ios-tech-challengeTests.swift
@@ -0,0 +1,35 @@
+//
+//  ios-tech-challengeTests.swift
+//  ios-tech-challenge
+//
+//  Created by Ammad Akhtar on 16/09/2018.
+//  Copyright © 2018 Ammad Akhtar. All rights reserved.
+//
+
+import XCTest
+
+class ios_tech_challengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
